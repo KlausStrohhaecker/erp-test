2926ab5a325a3e0c2fe4b198a186787d9292b215
KlausStrohhaecker <42267360+KlausStrohhaecker@users.noreply.github.com>
2021-03-20 16:01:07 +0100

    Update erp.c
    fix clock freq

--- firmware/src/application/src/erp/erp.c
+++ firmware/src/application/src/erp/erp.c
@@ -5,13 +5,13 @@
 
 void ERP_Init(void)
 {
-#define ADC_SETUP                                                                           \
-  ((51 << ADC0_CR_CLKDIV_Pos)  /* 204MHz / 51 --> 4MHz */                                   \
-   | (1 << ADC0_CR_BURST_Pos)  /* burst mode, continuous read */                            \
-   | (0 << ADC0_CR_CLKS_Pos)   /* "11" clocks --> 10 bits resolution, 363kHz sample rate */ \
-   | (1 << ADC0_CR_PDN_Pos)    /* enable ADC */                                             \
-   | (0 << ADC0_CR_START_Pos)) /* start = 0 required for burst mode */
-                               // effective sample rate per ERP is 363kHz / 4 = 90.9kHz
+#define ADC_SETUP                                                                               \
+  (((3 - 1) << ADC0_CR_CLKDIV_Pos) /* 12MHz / 3 --> 4MHz */                                     \
+   | (1 << ADC0_CR_BURST_Pos)      /* burst mode, continuous read */                            \
+   | (0 << ADC0_CR_CLKS_Pos)       /* "11" clocks --> 10 bits resolution, 363kHz sample rate */ \
+   | (1 << ADC0_CR_PDN_Pos)        /* enable ADC */                                             \
+   | (0 << ADC0_CR_START_Pos))     /* start = 0 required for burst mode */
+                                   // effective sample rate per ERP is 363kHz / 4 = 90.9kHz
 
   LPC_ADC0->CR = (0b00001111 << ADC0_CR_SEL_Pos)  // ADC0 scans Wiper 1 (@lane 0..3) of all ERPs
       | ADC_SETUP;
